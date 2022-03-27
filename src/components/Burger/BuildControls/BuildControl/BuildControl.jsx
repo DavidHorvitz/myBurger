60058aa00b49651bbd36a78885a3bc0a5116dd81
DavidHorvitz <2205dzh@gmail.com>
2022-03-27 09:38:48 +0300

    3
    a

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from './BuildControl.css';
+const buildControl = (props) =>(
+<div className="BuildControl">
+    <div className="Label">{props.label}</div>
+    <button className="Lass">Lass</button>
+    <button className="More">More</button>
+</div>
+);
+export default buildControl;
+
+// abcd
